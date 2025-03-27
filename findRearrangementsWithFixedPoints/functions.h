b02395967adbb8e18efa869d9574eabef4a5b191
ная <nastya034rus@yandex.ru>
2025-03-27 22:33:27 +0300

    pseudocode for the recursive function and for the main function(version 1)

--- findRearrangementsWithFixedPoints/functions.h
+++ findRearrangementsWithFixedPoints/functions.h
@@ -9,6 +9,6 @@ using namespace std;
 * \param[in] used_elements - контейнер с уже использованными числами
 * \param[in] max_fixed_points - максимум неподвижных точек которые могут быть в перестановке
 * \param[in] fixed_points_count - счетчик неподвижных точек в перестановке
-* \param[in] generated_permutations - контейнер с уже сгенерированными перестановками, который мы постоянно будем пополнять с каждым вызовом рекурсии(в теории)
+* \param[in] generated_permutations - контейнер с уже сгенерированными перестановками, который мы постоянно будем пополнять с каждым вызовом рекурсии
 */
 void recursion_for_rearrangements(vector<int>& current_permutation, int index, vector<bool>& used_elements, int max_fixed_points, int fixed_points_count, vector<vector<int>>& generated_permutations);
