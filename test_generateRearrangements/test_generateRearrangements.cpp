72f6ec3435186252c67abb0cdc8cf66179653426
ная <nastya034rus@yandex.ru>
2025-05-15 18:33:34 +0300

    created empty test project for generateRearrangements

---
+++ test_generateRearrangements/test_generateRearrangements.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../findRearrangementsWithFixedPoints/functions.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace testgenerateRearrangements
+{
+	TEST_CLASS(testgenerateRearrangements)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
