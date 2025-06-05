ff531576329787a3e830bbaca6a9ca142dec0734
ная <nastya034rus@yandex.ru>
2025-06-05 15:55:53 +0300

    empty test project for splitString function

---
+++ test_splitString/test_splitString.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../findRearrangementsWithFixedPoints/functions.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace testsplitString
+{
+	TEST_CLASS(testsplitString)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
