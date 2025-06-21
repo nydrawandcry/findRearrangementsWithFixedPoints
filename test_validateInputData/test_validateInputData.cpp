3ebedf8b1ac42ca0d913f867d3bf1f485808df7a
ная <nastya034rus@yandex.ru>
2025-06-21 04:39:26 +0300

    created empty test project for the validateInputData function

---
+++ test_validateInputData/test_validateInputData.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace testvalidateInputData
+{
+	TEST_CLASS(testvalidateInputData)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
