5dc91727d1876be8f58f83b37c28506237fb75bd
ная <nastya034rus@yandex.ru>
2025-06-21 04:54:49 +0300

    created tests for the validateInputData function

--- test_validateInputData/test_validateInputData.cpp
+++ test_validateInputData/test_validateInputData.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../findRearrangementsWithFixedPoints/functions.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,9 +10,114 @@ namespace testvalidateInputData
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(CorrectInput)
 		{
+			string input_string = "n=5, k=2";
+			vector<string> string_elements = {};
 
+			DataErrors errortype = validateInputData(input_string, string_elements);
+			DataErrors exp_errortype = NO_DATA_ERROR;
+
+			Assert::AreEqual(exp_errortype, errortype);
+		}
+
+		TEST_METHOD(WrongSymbolsCount)
+		{
+			string input_string = "n=5, k=";
+			vector<string> string_elements = {};
+
+			DataErrors errortype = validateInputData(input_string, string_elements);
+			DataErrors exp_errortype = TOO_MANY_SYMBOLS;
+
+			Assert::AreEqual(exp_errortype, errortype);
+		}
+
+		TEST_METHOD(ParametersAreNotInt)
+		{
+			string input_string = "n=a, k=e";
+			vector<string> string_elements = {};
+
+			DataErrors errortype = validateInputData(input_string, string_elements);
+			DataErrors exp_errortype = NO_INT;
+
+			Assert::AreEqual(exp_errortype, errortype);
+		}
+
+		TEST_METHOD(ParametersAreEmpty)
+		{
+			string input_string = "n= , k= ";
+			vector<string> string_elements = {};
+
+			DataErrors errortype = validateInputData(input_string, string_elements);
+			DataErrors exp_errortype = TOO_MANY_SYMBOLS;
+
+			Assert::AreEqual(exp_errortype, errortype);
+		}
+
+		TEST_METHOD(WrongParametersNames)
+		{
+			string input_string = "a=5, b=7";
+			vector<string> string_elements = {};
+
+			DataErrors errortype = validateInputData(input_string, string_elements);
+			DataErrors exp_errortype = WRONG_INPUT;
+
+			Assert::AreEqual(exp_errortype, errortype);
+		}
+
+		TEST_METHOD(ManySeparators)
+		{
+			string input_string = "n==5, k=2";
+			vector<string> string_elements = {};
+
+			DataErrors errortype = validateInputData(input_string, string_elements);
+			DataErrors exp_errortype = TOO_MANY_SYMBOLS;
+
+			Assert::AreEqual(exp_errortype, errortype);
+		}
+
+		TEST_METHOD(SpecialSymbolsInStr)
+		{
+			string input_string = "n_=5, k=2";
+			vector<string> string_elements = {};
+
+			DataErrors errortype = validateInputData(input_string, string_elements);
+			DataErrors exp_errortype = TOO_MANY_SYMBOLS;
+
+			Assert::AreEqual(exp_errortype, errortype);
+		}
+
+		TEST_METHOD(WrongParametersNamesRegister)
+		{
+			string input_string = "N=5, K=2";
+			vector<string> string_elements = {};
+
+			DataErrors errortype = validateInputData(input_string, string_elements);
+			DataErrors exp_errortype = WRONG_INPUT;
+
+			Assert::AreEqual(exp_errortype, errortype);
+		}
+
+		TEST_METHOD(ManyWhiteSeparators)
+		{
+			string input_string = "n=5,\tk=2    ";
+			vector<string> string_elements = {};
+
+			DataErrors errortype = validateInputData(input_string, string_elements);
+			DataErrors exp_errortype = NO_DATA_ERROR;
+
+			Assert::AreEqual(exp_errortype, errortype);
+		}
+
+		TEST_METHOD(ComplexTest)
+		{
+			string input_string = "N=5, k=a";
+			vector<string> string_elements = {};
+
+			DataErrors errortype = validateInputData(input_string, string_elements);
+			DataErrors exp_errortype = WRONG_INPUT;
+
+			Assert::AreEqual(exp_errortype, errortype);
 		}
 	};
 }
